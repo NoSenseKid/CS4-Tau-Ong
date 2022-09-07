da4614a4d389795c7dccac98ad939a1cc6de1f31
NoSenseKid <93103852+NoSenseKid@users.noreply.github.com>
2022-09-07 08:57:54 +0800

    Add files via upload

---
+++ Q1Ex1/Q1Ex1.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaproject1;
+    import java.util.Scanner;
+/**
+ *
+ * @author TAU
+ */
+public class JavaProject1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String codename1 = "Scared Cat";
+        String codename2 = "Hunter";
+        String codename3 = "Extinction";
+        
+        String type1 = "Black Bear";
+        String type2 = "Brown Bear";
+        String type3 = "Polar Bear";      
+     
+        int age1 = 37;
+        int age2 = 39;
+        int age3 = 48;
+        
+        String escOpt1 = "Taunt";
+        String escOpt2 = "Play Dead";
+        String escOpt3 = "No Escape";
+        
+        System.out.printf();
+        
+    }
+    
+}
